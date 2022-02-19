25650422c466c0719d9c3590ee352494183c8209
Princewill Olaiya <princewillolaiya@gmail.com>
2022-02-19 21:02:22 +0100

    purchases app skeleton

---
+++ purchases/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import get_user_model
+
+from tickets.models import Ticket
+# Create your models here.
+
+User = get_user_model()
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(
+        Ticket, null=True, blank=True, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.first_name + ": " + self.ticket.event.name
+
+    def save(self, *args, **kwargs):
+        self.ticket.total = self.ticket.total - self.quantity
+        self.ticket.save()
