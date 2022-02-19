e3ff11cff6bdb6f1248b1e6de72e14ac65d70b43
Princewill Olaiya <princewillolaiya@gmail.com>
2022-02-19 21:43:45 +0100

    auth urls and views

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,4 @@ urlpatterns = [
     path('register/', include('rest_auth.registration.urls')),
     path('login/', views.LoginView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
-    # path('registration/account-confirm-email/')
-    #path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    # path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    # path('register/', include('dj_rest_auth.registration.urls'))
-    # path('register/', views.CustomRegisterView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,3 @@
-from django.shortcuts import render
-from rest_auth.registration.views import RegisterView, LoginView
-from .serializers import RegistrationSerializer, LoginSerializer
-from rest_framework import permissions
+# from django.shortcuts import render
+
 # Create your views here.
-
-
-class Login(LoginView):
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, *args, **kwargs):
-        print("Haba")
-        serializer = LoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        print("Serializer: ", serializer)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return super().post(request, format=None)
