151c426a1992831631a6be7668c069f1b136f34c
Princewill Olaiya <princewillolaiya@gmail.com>
2022-03-03 04:36:53 +0100

    made some fields read only

--- purchases/serializers.py
+++ purchases/serializers.py
@@ -6,4 +6,7 @@ class PurchaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Purchase
         fields = ("__all__")
-        read_only_fields = ("user", )
+        read_only_fields = (
+            "user", "slug", "url",
+            "used", "qrcode"
+        )
